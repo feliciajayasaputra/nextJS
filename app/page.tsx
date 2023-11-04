0f392bcd7e5408c53f69e47cadb68c06f8dede1e
feliciajayasaputra <feljayasaputra@gmail.com>
2023-11-04 12:17:10 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import CountButton from "./CountButton"
+export default function Page() {
+    return <div>
+    <CountButton />
+    <CountButton />
+    <CountButton />
+    <hr />
+    <a href="github.com">my github repo</a>
+    <hr />
+    <p>Search Bar</p>
+    </div>
+  }
+
+
